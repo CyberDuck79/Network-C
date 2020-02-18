54c2f982326be04b9c77a5cb90cf5301a3edf950
fhenrion <fhenrion@e3r8p17.42.fr>
2020-02-18 10:50:34 +0100

    save

--- ftp/cmd_parsing.c
+++ ftp/cmd_parsing.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cmd_ini.h                                          :+:      :+:    :+:   */
+/*   cmd_parsing.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/18 08:48:17 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/02/18 10:46:10 by fhenrion         ###   ########.fr       */
+/*   Created: 2020/02/18 10:47:06 by fhenrion          #+#    #+#             */
+/*   Updated: 2020/02/18 10:48:33 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CMD_INI_H
-# define CMD_INI_H
-
-#include "server.h"
-
-/*Typedef commands array*/
-typedef	void (*t_exec_cmd)(t_connection *client, char data[BUFF_SIZE]);
-
-/*Commands array initialization array*/
-void	cmd_ini(t_exec_cmd cmd_tab[CMD_TAB_LEN]);
-
-#endif
+#include "cmd_parsing.h"
