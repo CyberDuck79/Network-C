4f56e2aca3ae56a921d2e02ece13ed9bedc64cc1
Flavien Henrion <flavien.henrion@protonmail.com>
2020-02-18 08:59:17 +0100

    first save

--- ftp/cmd_ini.h
+++ ftp/cmd_ini.h
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cmd.h                                              :+:      :+:    :+:   */
+/*   cmd_ini.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/18 00:29:06 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/02/18 08:54:39 by fhenrion         ###   ########.fr       */
+/*   Created: 2020/02/18 08:48:17 by fhenrion          #+#    #+#             */
+/*   Updated: 2020/02/18 08:55:04 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CMD_H
-# define CMD_H
+#ifndef CMD_INI_H
+# define CMD_INI_H
 
-# include "server.h"
+#include "server.h"
 
-void	cmd_ls(t_connection *client, char data[BUFF_SIZE]);
-void	cmd_get(t_connection *client, char data[BUFF_SIZE]);
-void	cmd_put(t_connection *client, char data[BUFF_SIZE]);
-void	cmd_pwd(t_connection *client, char data[BUFF_SIZE]);
-void	cmd_quit(t_connection *client, char data[BUFF_SIZE]);
-void	cmd_unknown(t_connection *client, char data[BUFF_SIZE]);
+typedef	void (*t_exec_cmd)(t_connection *client, char data[BUFF_SIZE]);
+void	cmd_ini(t_exec_cmd cmd_tab[CMD_TAB_LEN]);
 
 #endif
--- ftp/server.h
+++ ftp/server.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ftp.h                                              :+:      :+:    :+:   */
+/*   server.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 22:46:52 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/02/18 00:50:39 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/02/18 08:58:38 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 /*FTP server*/
 # include <sys/socket.h>
 # include <netinet/in.h>
-# include <string.h>
-# include <stdio.h>
 # include <stdlib.h>
-# include <errno.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
 /*for getting file size using stat()*/
 # include <sys/stat.h>
 /*for sendfile()*/
@@ -28,13 +28,13 @@
 # include <sys/uio.h>
 /*for O_RDONLY*/
 # include <fcntl.h>
+#include "cmd_ini.h"
 
-# define ERROR -1
-# define BUFF_SIZE 256
+/*Global Define*/
+# define ERROR			-1
+# define BUFF_SIZE		256
 
-typedef struct stat		t_stat;
-typedef struct sockaddr	t_addr;
-
+/*Implemented commands list*/
 typedef enum			e_cmd
 {
 	LS,
@@ -46,20 +46,22 @@ typedef enum			e_cmd
 	UNKNOWN
 }						t_cmd;
 
-# define CMD_TAB_LEN 7
-# define CMD_STR_TAB {"ls", "get", "put", "pwd", "bye", "quit", ""}
-# define CMD_LEN_TAB {2, 3, 3, 3, 3, 4, 1}
-# define CMD_TAB {LS, GET, PUT, PWD, BYE, QUIT, UNKNOWN}
+/*Command parsing array*/
+# define CMD_TAB_LEN	7
+# define CMD_STR_TAB	{"ls", "get", "put", "pwd", "bye", "quit", ""}
+# define CMD_LEN_TAB 	{2, 3, 3, 3, 3, 4, 0}
+# define CMD_TAB		{LS, GET, PUT, PWD, BYE, QUIT, UNKNOWN}
 
+/*Shortcut typedef*/
+typedef struct stat		t_stat;
+typedef struct sockaddr	t_addr;
+
+/*Networking struct*/
 typedef struct			s_connection
 {
 	struct sockaddr_in	addr;
 	int					sock;
 	socklen_t			len;
 }						t_connection;
-
-typedef void			(*t_exec_cmd)(t_connection *client, char data[BUFF_SIZE]);
-
-void					cmd_ini(t_exec_cmd cmd_tab[CMD_TAB_LEN]);
 
 #endif
