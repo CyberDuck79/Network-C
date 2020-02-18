c0ab396dcc0858e73be580460856277c1596f5a9
Flavien Henrion <flavien.henrion@protonmail.com>
2020-02-19 00:53:06 +0100

    client to change

--- ftp/client/client.c
+++ ftp/client/client.c
@@ -26,7 +26,7 @@ int main(int argc,char *argv[])
 	int k, size, status;
 	int filehandle;
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
+	if (sock == -1)
 	{
 		printf("socket creation failed");
 		exit(1);
@@ -35,17 +35,16 @@ int main(int argc,char *argv[])
 	server.sin_port = atoi(argv[1]);
 	server.sin_addr.s_addr = 0;
 	k = connect(sock,(struct sockaddr*)&server, sizeof(server));
-	if(k == -1)
+	if (k == -1)
 	{
 		printf("Connect Error");
 		exit(1);
 	}
-	int i = 1;
-	while(1)
+	while (1)
 	{
 		printf("Enter a choice:\n1- get\n2- put\n3- pwd\n4- ls\n5- cd\n6- quit\n");
 		scanf("%d", &choice);
-		switch(choice)
+		switch (choice)
 		{
 		case 1:
 			printf("Enter filename to get: ");
@@ -54,19 +53,19 @@ int main(int argc,char *argv[])
 			strcat(buf, filename);
 			send(sock, buf, 100, 0);
 			recv(sock, &size, sizeof(int), 0);
-			if(!size)
+			if (!size)
 			{
 				printf("No such file on the remote directory\n\n");
 				break;
 			}
 			f = malloc(size);
 			recv(sock, f, size, 0);
-			while(1)
+			while (1)
 			{
 				filehandle = open(filename, O_CREAT | O_WRONLY, 0644);
-				if(filehandle == -1)
+				if (filehandle == -1)
 				{
-					sprintf(filename + strlen(filename), "%d", i);//needed only if same directory is used for both server and client
+					sprintf(filename + strlen(filename), "%d", 1);//needed only if same directory is used for both server and client
 				}
 				else
 					break;
@@ -81,7 +80,7 @@ int main(int argc,char *argv[])
 			printf("Enter filename to put to server: ");
 			scanf("%s", filename);
 			filehandle = open(filename, O_RDONLY);
-			if(filehandle == -1)
+			if (filehandle == -1)
 			{
 				printf("No such file on the local directory\n\n");
 				break;
@@ -94,7 +93,7 @@ int main(int argc,char *argv[])
 			send(sock, &size, sizeof(int), 0);
 			sendfile(sock, filehandle, 0, &size, NULL, 0);
 			recv(sock, &status, sizeof(int), 0);
-			if(status)
+			if (status)
 				printf("File stored successfully\n");
 			else
 				printf("File failed to be stored to remote machine\n");
@@ -123,7 +122,7 @@ int main(int argc,char *argv[])
 			scanf("%s", buf + 3);
 			send(sock, buf, 100, 0);
 			recv(sock, &status, sizeof(int), 0);
-			if(status)
+			if (status)
 				printf("Remote directory successfully changed\n");
 			else
 				printf("Remote directory failed to change\n");
@@ -132,7 +131,7 @@ int main(int argc,char *argv[])
 			strcpy(buf, "quit");
 			send(sock, buf, 100, 0);
 			recv(sock, &status, 100, 0);
-			if(status)
+			if (status)
 			{
 				printf("Server closed\nQuitting..\n");
 				exit(0);
